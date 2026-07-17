17cba6778420fae55872d473cba5f7481f9d5852
Jonas Remmert <j.remmert@mailbox.org>
2026-07-18 00:11:35 +0200

    ci: use west sdk --gnu-toolchains flag
    --toolchains is deprecated; replace it in CI and Codespaces setup.

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -11,4 +11,4 @@ fi
 west update --narrow -o=--depth=1
 pip install -r zephyr/scripts/requirements.txt --break-system-packages
 west zephyr-export
-west sdk install --toolchains arm-zephyr-eabi x86_64-zephyr-elf
+west sdk install --gnu-toolchains arm-zephyr-eabi x86_64-zephyr-elf
