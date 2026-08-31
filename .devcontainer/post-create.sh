69ea24e718e83e6072ca606032754a205f2a830b
Andreas Kurz <andreas.kurz@inovex.de>
2026-08-31 16:48:53 +0200

    feat: add clangd support
    This will add clangd and its extension to provide proper syntax
    highlighting. The .clangd file must be present in the workspace's root
    so that the plugin picks up the configuration. Per default it uses the
    build/compile-commands.json as an input so it is working only when a
    build has been performed.

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -11,3 +11,6 @@ west update --narrow -o=--depth=1
 pip install -r zephyr/scripts/requirements.txt --break-system-packages
 west zephyr-export
 west sdk install --toolchains arm-zephyr-eabi x86_64-zephyr-elf
+
+# make clangd file visible for the plugin
+ln -s zephyr-workshop/.devcontainer/.clangd
